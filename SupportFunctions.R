6b6fec40d1857c3b0056636003807cfe143d39dc
Fabian Held <Fabian.Held@sydney.edu.au>
2014-11-07 13:43:55 +1100

    pasted string manipulation functions

---
+++ SupportFunctions.R
@@ -0,0 +1,37 @@
+Isolate.Participants <- function(AString){
+                                          # assume x a concatenated string of names
+                                          x <- AString
+                                          if (AString != ""){
+                                          #x <- CPCNodesNames$Participants[14]
+                                          x <- gsub(x, pattern=";#[0-9]+", replacement="")
+
+                                          x <- gsub(x, pattern=" +,", replacement=",")
+                                          x <- gsub(x, pattern="\t", replacement="", fixed=TRUE)
+
+                                          #x <- gsub(x, pattern="[a-z]+;#|[a-z]+ ;#", replacement="\\.;;")
+                                          #x <- gsub(x, pattern="[a-z]+$|[a-z]+ $", replacement="\\.")
+                                          x <- gsub(x, pattern=" , ", replacement=", ")
+                                          x <- gsub(x, pattern=" $", replacement="")
+                                          participants.list <- strsplit(x, split=";#")[[1]]
+                                          return(data.frame(Leader = FALSE, Participant = TRUE, Researcher =  participants.list))
+                                          }
+                                          #else{participants.list <- ""}
+                                         }
+
+Isolate.Leaders <- function(AString){
+                                     #assume x a concatenated string of names
+                                     if (AString != ""){
+                                     x <- AString
+                                     #x <- CPCNodesNames$Leader[14]
+                                     x <- gsub(x, pattern=";#[0-9]+", replacement="")
+                                     x <- gsub(x, pattern=" +,", replacement=",")
+                                     x <- gsub(x, pattern="\t", replacement="", fixed=TRUE)
+                                     #x <- gsub(x, pattern="[a-z]+;#|[a-z]+ ;#", replacement="\\.;;")
+                                     #x <- gsub(x, pattern="[a-z]+$|[a-z]+ $", replacement="\\.")
+                                     x <- gsub(x, pattern=" , ", replacement=", ")
+                                     x <- gsub(x, pattern=" $", replacement="")
+                                     Leader.list <- strsplit(x, split=";#")[[1]]
+                                     return(data.frame(Leader = TRUE, Participant = FALSE, Researcher = Leader.list))
+                                     }
+                                     #else{Leader.list <- ""}
+                                     }
